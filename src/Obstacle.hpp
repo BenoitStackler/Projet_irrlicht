c159ec37c0e26b560e452973f12577ffef720e88
Benoit Stackler <51323792+BenoitStackler@users.noreply.github.com>
2021-01-09 14:22:32 +0100

    merge versions

--- src/Obstacle.hpp
+++ src/Obstacle.hpp
@@ -13,20 +13,54 @@ public:
     Obstacle(irr::core::vector2d<int> position, irr::f32 size);
     Obstacle(irr::core::vector2d<int> position, irr::f32 size, irr::core::vector3df rotation);
 
-    //Getters
+    Obstacle(int pos_x,int pos_y);                  //Position de l'obstacle définie et dimensions par défaut (1,1)
+    Obstacle(int pos_x, int pos_y, int nx_param, int ny_param);       //Position et dimensions définies
+
+    //Getters  
+    //Position de l'obstacle
+    const int x() const;        //Getter de position en x de l'obstacle
+    const int y() const;        //Getter de position en y de l'obstacle
+
+    //Dimensions de l'obstacle
+    const int nx() const;       //Getter de dimension en x de l'obstacle
+    const int ny() const;       //Getter de dimension en y de l'obstacle
+
+    const int id() const;       //Getter d'id de l'obstacle
+
+    //Setters
+    //Position de l'obstacle
+    void x(int pos_x);      
+    void y(int pos_y);
+
+    //Dimensions de l'obstacle
+    void nx(int len);
+    void ny(int height);
+  
     //Position de l'obstacle
     irr::core::vector2d<int> const position() const;
-    std::string type(); //Retourne une string donnant le type d'obstacle(mur ou caisse)
+    std::string type();     //Retourne une string donnant le type d'obstacle(mur ou caisse)
+    void draw();            //Fontion de creation du node irrlicht 
 
     void position(irr::core::vector2d<int> position);
     void rotation(irr::core::vector3df rotation);
-    void scale(irr::core::vector3df scale);
 
 private:
-    irr::core::vector2d<int> m_position;
-    irr::core::vector3df m_rotation;
-    irr::f32 m_size;
-    irr::scene::ISceneNode *m_node;
-};
 
+    irr::core::vector2d<int> m_position;
+    irr::core::vector3df     m_rotation;
+    irr::f32                 m_size;
+    irr::scene::ISceneNode*  m_node;
+
+    //Attributs de position du point haut gauche de l'obstacle 
+    int m_x;      //Position en x du point haut gauche de l'obstacle (en nombre de case de grille)
+    int m_y;      //Position en y du point haut gauche du de l'obstacle (en nombre de case de grille)
+
+    //Attributs de dimentions de l'obstacle
+    int m_nx;     //Longueur de l'obstacle (en nombre de case de grille)
+    int m_ny;     //Largeur de l'obstacle (en nombre de case de grille)
+    
+    int m_height;
+    
+    int m_id;     //Identifiant de l'obstacle
+};
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,19 @@
 #include <irrlicht.h>
+#include <iostream>
+#include <fstream>
+#include <vector>
+
 #include "character.hpp"
 #include "hero.hpp"
 #include "enemy.hpp"
 #include "projectile.hpp"
 #include "Terrain.hpp"
 #include "Caisse.hpp"
-
+#include "Obstacle.hpp"
+#include "Grid.hpp"
+#include "Node_Graph.hpp"
+#include "Path.hpp"
+#include "vec2.hpp"
 using namespace irr;
 using namespace core;
 using namespace scene;
@@ -78,7 +86,7 @@ int main()
   Terrain terain = Terrain("./irrlicht-1.8.4/media/terrain-heightmap.bmp", "./irrlicht-1.8.4/media/stones.jpg", vector3df(0.0f, 0.0f, 0.0f), vector3df(0, 0, 0), vector3df(1.0f, 0, 1.0f));
   Caisse caisse = Caisse(vector2d<int>(10, 10), 10.0f);
   Caisse caisse1 = Caisse(vector2d<int>(120, 120), 10.0f);
-  caisse1.scale(vector3df(3.0f, 1.0f, 1.0f));
+  //caisse1.scale(vector3df(3.0f, 1.0f, 1.0f));
   Hero hero = Hero("./irrlicht-1.8.4/media/sydney.md2", "./irrlicht-1.8.4/media/sydney.bmp", vector3df(0, 0, 0), vector3df(0, 0, 0), 200.0f, 20.0f);
   std::vector<Enemy> enemies = create_enemy(3, hero);
 
@@ -114,6 +122,105 @@ int main()
 
   // This is the movemen speed in units per second.
   const f32 MOVEMENT_SPEED = 40.f;
+
+
+  int Nx = 10;
+  int Ny = 10;
+
+  Obstacle obst1;
+  obst1.x(1);
+  obst1.y(2);
+  obst1.nx(2);
+  obst1.ny(1);
+
+  Obstacle obst2;
+  obst2.x(6);
+  obst2.y(6);
+  obst2.nx(3);
+  obst2.ny(2);
+
+  Obstacle obst3;
+  obst3.x(5);
+  obst3.y(4);
+  obst3.nx(2);
+  obst3.ny(2);
+
+  Obstacle obst4;
+  obst4.x(3);
+  obst4.y(6);
+  obst4.nx(2);
+  obst4.ny(2);
+
+
+  std::vector<Obstacle> obstacles;
+  obstacles.push_back(obst1);
+  obstacles.push_back(obst2);
+  obstacles.push_back(obst3);
+  obstacles.push_back(obst4);
+
+  Grid grid = create_grid_obstacles(Nx, Ny, obstacles);
+
+
+
+  std::vector<vec2> nodes = get_nodes_positions(Nx, Ny, obstacles, grid);
+  Grid grid_nodes(Nx, Ny);
+
+  for (int k = 0 ; k < nodes.size() ; k++){
+      grid_nodes(nodes[k].x, nodes[k].y) = 1;
+  }
+
+  for (int i = 0 ; i < Nx ; i++){
+      for (int j = 0 ; j < Ny ; j++){
+          std::cout << grid(i, j) + 2*grid_nodes(i, j) << " " ;
+
+      }
+      std::cout << std::endl;
+  }
+
+  vec2 p0;
+  vec2 p1;
+  p0.x = 0;
+  p1.x = 2;
+  p0.y = 3;
+  p1.y = 5;
+
+  auto c = bresenham(p0, p1);
+
+
+
+  std::vector<Node_Graph> vec_nodes;
+  int k = 0;
+  for (vec2 node_v : nodes){
+      vec_nodes.push_back(Node_Graph(node_v.x, node_v.y, k));
+      ++k;
+  } 
+  Node_Graph start(7, 4, k);
+  ++k;
+  Node_Graph end(1, 9, k);
+
+
+  vec_nodes.push_back(end);
+  vec_nodes.push_back(start);
+
+  start.compute_neighbours(vec_nodes, grid);
+  end.compute_neighbours(vec_nodes, grid);
+
+  for (int k = 0 ; k < vec_nodes.size() ; k++){
+      vec_nodes[k].compute_neighbours(vec_nodes, grid);
+  }
+
+  int s = vec_nodes.size();
+
+  Path foundPath = find_path(vec_nodes, start, end);
+
+  for (Node_Graph node : foundPath.path()){
+      std::cout << '(' << node.x() << ',' << node.y() <<')' << std::endl;
+  }
+
+
+
+
+
 
   while (device->run())
   {
